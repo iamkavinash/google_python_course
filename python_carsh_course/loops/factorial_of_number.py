ebe802af48f476b274182c2e6b52de5d980e006e
Avinash <avinash@peppo.co.in>
2022-05-30 19:09:58 +0530

    added more loop programs

--- python_carsh_course/loops/factorial_of_number.py
+++ python_carsh_course/loops/factorial_of_number.py
@@ -7,3 +7,14 @@ def factorial(n):
 
 print(factorial(4)) # should return 24
 print(factorial(5)) # should return 120
+####################
+#print the first 10 factorials (from 0 to 9) with the corresponding number
+
+def factorial(n):
+    result = 1
+    for x in range(1,n+1):
+        result = x * result
+    return result
+
+for n in range(0,10):
+    print(n, factorial(n))
