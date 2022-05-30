bbf4f4b406e210617f20d66f140a185d235c0b5c
Avinash Chowdary Karapakula <40687689+iamkavinash@users.noreply.github.com>
2022-05-30 14:21:51 +0530

    added loops

---
+++ python_carsh_course/loops/while_loop.py
@@ -0,0 +1,5 @@
+x = int(input("Enter the number:"))
+c = 0
+while c <x:
+    print(str(c))
+    c+=1
---
+++ python_carsh_course/loops/while_with_func.py
@@ -0,0 +1,10 @@
+n = int(input("Enter the number attempts:"))
+def attempts(n):
+    x=1
+    while x<=n:
+        print("Attempt " + str(x))
+        x+=1
+    print("Done")
+attempts(n)
+
+
